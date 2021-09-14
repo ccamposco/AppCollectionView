b0c1666aaad14974c8fbc59533280af6f72ae2f4
CESAR CARLOS CAMPOS CONTRERAS <ccamposco@senati.pe>
2021-09-14 12:09:37 -0500

    Add project files.

---
+++ AppCollectionView/Models/Students.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppCollectionView.Models
+{
+    public class Students
+    {
+        public string Name { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Telephone { get; set; }
+    }
+}
---
+++ AppCollectionView/ViewModels/StudentsViewModel.cs
@@ -0,0 +1,33 @@
+﻿using AppCollectionView.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace AppCollectionView.ViewModels
+{
+    public class StudentsViewModel
+    {
+        public ObservableCollection<Students> students { get; set; }
+
+        public StudentsViewModel()
+        {
+            students = new ObservableCollection<Students>
+            {
+                new Students{Name="María", LastName="Rojas", Telephone = "(01) 980930897"},
+                new Students{Name="José", LastName="Rojas", Telephone = "(01) 980930897"},
+                new Students{Name="Carlos", LastName="Rojas", Telephone = "(01) 980930897"},
+                new Students{Name="Mario", LastName="Rojas", Telephone = "(01) 980930897"},
+                new Students{Name="Roberto", LastName="Rojas", Telephone = "(01) 980930897"},
+                new Students{Name="Gloria", LastName="Rojas", Telephone = "(01) 980930897"},
+                new Students{Name="Rosario", LastName="Rojas", Telephone = "(01) 980930897"},
+                new Students{Name="Liliana", LastName="Rojas", Telephone = "(01) 980930897"},
+                new Students{Name="Guisela", LastName="Rojas", Telephone = "(01) 980930897"},
+                new Students{Name="Yessica", LastName="Rojas", Telephone = "(01) 980930897"},
+                new Students{Name="Luis", LastName="Rojas", Telephone = "(01) 980930897"},
+
+
+            };
+        }
+    }
+}
